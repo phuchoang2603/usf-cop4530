3c3196fa5fc4a527c3ef127c75f6ff594fdcd2bb
felix <xuanphuc.a1gv@gmail.com>
2026-02-04 04:23:38 -0500

    add cpp flake

---
+++ assignment1/section1-q3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    int count = 10; 
+    double price = 25.75; 
+    
+    cout << "Count: " << count << endl;
+    cout << "Price: " << price << endl;
+    
+    return EXIT_SUCCESS;
+}
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "C++ development environment with Clang and CMake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        # Use a Clang-based standard environment
+        stdenv = pkgs.clangStdenv;
+      in
+      {
+        devShells.default = pkgs.mkShell.override { inherit stdenv; } {
+          nativeBuildInputs = with pkgs; [
+            cmake
+            cmake-language-server
+            ninja # Faster builds than Make
+            pkg-config # Helps find libraries
+            clang-tools # Provides clangd, clang-format, clang-tidy
+            gdb # Debugger
+            llvmPackages.lldb # LLVM Debugger
+          ];
+
+          shellHook = ''
+            echo "C++ Development Shell (Clang)"
+            # Generates a compile_commands.json for clangd if using CMake
+            export CMAKE_EXPORT_COMPILE_COMMANDS=1
+          '';
+        };
+      }
+    );
+}
